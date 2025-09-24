1061b3c68c35d2152c9635debd0c69f4883f4632
nurdsaga16 <nurd.saga14@gmail.com>
2025-09-24 09:47:55 +0500

    first commit

---
+++ src/test/java/com/nurdaulet/reservation/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurdaulet.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
